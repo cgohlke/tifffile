8339a30b7d1665413ef1b7795d4fc259a2775aa9
Christoph Gohlke <cgohlke@uci.edu>
2018-10-19 23:17:24 -0700

    Update tifffile/__init__.py

--- tifffile/__init__.py
+++ tifffile/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # tifffile/__init__.py
 
-from .tifffile import __doc__, __all__, __version__, main  # noqa
-from .tifffile import *  # noqa
+from .tifffile import __doc__, __all__, __version__, main
+from .tifffile import *
