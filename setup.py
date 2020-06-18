906c87fa7f9a36371342cf4db340677e8a5d6fd5
Christoph Gohlke <cgohlke@uci.edu>
2020-06-18 14:23:48 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -92,7 +92,6 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
