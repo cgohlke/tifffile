45ca2b650a13ba7f60ce7be509748370321717fd
Christoph Gohlke <cgohlke@uci.edu>
2021-01-11 15:41:56 -0800

    Update tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,11 @@
 import os
 import sys
 
+if os.environ.get('VSCODE_CWD'):
+    # work around pytest not using PYTHONPATH in VSCode
+    sys.path.insert(
+        0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    )
 
 if os.environ.get('SKIP_CODECS', None):
     sys.modules['imagecodecs'] = None
