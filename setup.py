e7865bc48eb26a02f081633884e479e7880ed6ac
Christoph Gohlke <cgohlke@cgohlke.com>
2022-07-28 12:17:08 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 # tifffile/setup.py
 
-"""Tifffile package setuptools script."""
+"""Tifffile package Setuptools script."""
 
 import sys
 import re
@@ -9,20 +9,28 @@ from setuptools import setup
 
 buildnumber = ''
 
-with open('tifffile/tifffile.py') as fh:
-    code = fh.read()
 
-version = re.search(r"__version__ = '(.*?)'", code).groups()[0]
+def search(pattern, code, flags=0):
+    # return first match for pattern in code
+    match = re.search(pattern, code, flags)
+    if match is None:
+        raise ValueError(f'{pattern!r} not found')
+    return match.groups()[0]
+
+
+with open('tifffile/tifffile.py') as fh:
+    code = fh.read().replace('\r\n', '\n').replace('\r', '\n')
+
+version = search(r"__version__ = '(.*?)'", code)
 version += ('.' + buildnumber) if buildnumber else ''
 
-description = re.search(r'"""(.*)\.(?:\r\n|\r|\n)', code).groups()[0]
+description = search(r'"""(.*)\.(?:\r\n|\r|\n)', code)
 
-readme = re.search(
+readme = search(
     r'(?:\r\n|\r|\n){2}r"""(.*)"""(?:\r\n|\r|\n){2}[__version__|from]',
     code,
     re.MULTILINE | re.DOTALL,
-).groups()[0]
-
+)
 readme = '\n'.join(
     [description, '=' * len(description)] + readme.splitlines()[1:]
 )
@@ -33,33 +41,27 @@ if 'sdist' in sys.argv:
     with open('README.rst', 'w') as fh:
         fh.write(readme)
 
-    license = re.search(
+    license = search(
         r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+r""',
         code,
         re.MULTILINE | re.DOTALL,
-    ).groups()[0]
-
+    )
     license = license.replace('# ', '').replace('#', '')
 
     with open('LICENSE', 'w') as fh:
         fh.write('BSD 3-Clause License\n\n')
         fh.write(license)
 
-    revisions = (
-        re.search(
-            r'(?:\r\n|\r|\n){2}(Revisions.*)   \.\.\.',
-            readme,
-            re.MULTILINE | re.DOTALL,
-        )
-        .groups()[0]
-        .strip()
-    )
+    revisions = search(
+        r'(?:\r\n|\r|\n){2}(Revisions.*)- \.\.\.',
+        readme,
+        re.MULTILINE | re.DOTALL,
+    ).strip()
 
     with open('CHANGES.rst', 'r') as fh:
         old = fh.read()
 
-    d = revisions.splitlines()[-1]
-    old = old.split(d)[-1]
+    old = old.split(revisions.splitlines()[-1])[-1]
     with open('CHANGES.rst', 'w') as fh:
         fh.write(revisions.strip())
         fh.write(old)
@@ -67,12 +69,12 @@ if 'sdist' in sys.argv:
 setup(
     name='tifffile',
     version=version,
+    license='BSD',
     description=description,
     long_description=readme,
     author='Christoph Gohlke',
-    author_email='cgohlke@uci.edu',
-    license='BSD',
-    url='https://www.lfd.uci.edu/~gohlke/',
+    author_email='cgohlke@cgohlke.com',
+    url='https://www.cgohlke.com',
     project_urls={
         'Bug Tracker': 'https://github.com/cgohlke/tifffile/issues',
         'Source Code': 'https://github.com/cgohlke/tifffile',
@@ -82,11 +84,11 @@ setup(
     python_requires='>=3.8',
     install_requires=[
         'numpy>=1.19.2',
-        # 'imagecodecs>=2021.11.20',
+        # 'imagecodecs>=2022.2.22',
     ],
     extras_require={
         'all': [
-            'imagecodecs>=2021.11.20',
+            'imagecodecs>=2022.2.22',
             'matplotlib>=3.3',
             'lxml',
             # 'zarr',
@@ -127,5 +129,6 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
