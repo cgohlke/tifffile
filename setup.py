fa8b4722b61879b7a1c13263bfb184ddf12a1b2f
Christoph Gohlke <cgohlke@cgohlke.com>
2024-07-02 20:46:45 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -150,9 +150,9 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
 )
