344b21878b349eca0957f145f4d38d9b1098a8f5
Christoph Gohlke <cgohlke@cgohlke.com>
2025-09-20 10:34:45 -0700

    Update tifffile/__init__.py

--- tifffile/__init__.py
+++ tifffile/__init__.py
@@ -2,3 +2,8 @@
 
 from .tifffile import *
 from .tifffile import __all__, __doc__, __version__, main
+
+# constants are repeated for documentation
+
+__version__ = __version__
+"""Tifffile version string."""
